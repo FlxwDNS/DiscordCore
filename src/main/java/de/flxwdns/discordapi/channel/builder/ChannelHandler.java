21014b4dee538b7a2a319d4f94e12b9bc442d688
FlxwDNS <friedrichsradik@gmail.com>
2023-02-11 15:42:27 +0100

    Return channel (#51281)

--- src/main/java/de/flxwdns/discordapi/channel/builder/ChannelHandler.java
+++ src/main/java/de/flxwdns/discordapi/channel/builder/ChannelHandler.java
@@ -2,11 +2,14 @@ package de.flxwdns.discordapi.channel.builder;
 
 import de.flxwdns.discordapi.DiscordCore;
 import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.channel.Channel;
 import discord4j.core.spec.TextChannelCreateSpec;
 
 public final class ChannelHandler {
 
-    public void createChannel(String guildId, TextChannelCreateSpec textChannel) {
-        DiscordCore.getClient().getGuildById(Snowflake.of(guildId)).block().createTextChannel(textChannel).subscribe();
+    public Channel createChannel(String guildId, TextChannelCreateSpec textChannel) {
+        var channel = DiscordCore.getClient().getGuildById(Snowflake.of(guildId)).block().createTextChannel(textChannel);
+        channel.subscribe();
+        return channel.block();
     }
 }
