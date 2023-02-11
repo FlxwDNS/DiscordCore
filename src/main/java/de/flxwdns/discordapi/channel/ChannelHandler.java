b616abcc6258d77ea081023ae048dd6a19c07864
FlxwDNS <friedrichsradik@gmail.com>
2023-02-11 19:56:11 +0100

    Implements getter (#61287)

--- src/main/java/de/flxwdns/discordapi/channel/ChannelHandler.java
+++ src/main/java/de/flxwdns/discordapi/channel/ChannelHandler.java
@@ -3,10 +3,19 @@ package de.flxwdns.discordapi.channel;
 import de.flxwdns.discordapi.DiscordCore;
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.GuildChannel;
 import discord4j.core.spec.TextChannelCreateSpec;
 import discord4j.core.spec.VoiceChannelCreateSpec;
 
 public final class ChannelHandler {
+
+    public GuildChannel getChannel(String guildId, String channelId) {
+        return DiscordCore.getClient().getGuildById(Snowflake.of(guildId)).block().getChannelById(Snowflake.of(channelId)).block();
+    }
+
+    public GuildChannel getChannelByName(String guildId, String channelName) {
+        return DiscordCore.getClient().getGuildById(Snowflake.of(guildId)).block().getChannels().filter(it -> it.getName().equalsIgnoreCase(channelName)).blockFirst();
+    }
 
     public Channel createChannel(String guildId, TextChannelCreateSpec textChannel) {
         return DiscordCore.getClient().getGuildById(Snowflake.of(guildId)).block().createTextChannel(textChannel).block();
